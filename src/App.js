984efe257efa5688b1243ab0abb95a698cffd29d
Will Morris <williamdunstanmorris@gmail.com>
2019-01-23 17:11:50 +0000

    Add three package to npm; add binding file inital commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import threeBind from './threejs/binder';
+
 
 class App extends Component {
+
+  /**
+   * Invoked immediately after a component is mounted (after inserted into the tree)
+   */
+  componentDidMount() {
+    threeBind(this.threeRootElement);
+  }
+
+  /**
+   * Top level component to render to the dom.
+   * @ref will return the specified threeRootElement specified
+   * @className lement with a specified property className, a React feature to grab css properties easily.
+   */
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="header-header" ref={element => this.threeRootElement = element} ></div>
     );
+
   }
 }
 
